59e88156f6e5eb730856836bc0c7b3191aa0ee40
Oscar Martinez <darshuhot@hotmail.com>
2026-04-14 20:42:58 +0200

    arreglo del login, actualizado encabezado CSP

--- vite.config.ts
+++ vite.config.ts
@@ -47,6 +47,13 @@ export default defineConfig(({mode}) => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
+      proxy: {
+        '/api': {
+          target: 'https://geomeasure-pro.onrender.com',
+          changeOrigin: true,
+          secure: true,
+        }
+      }
     },
   };
 });
